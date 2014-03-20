6e8a08e8efa51fc934e3af3a30d790225721828e
Ubuntu <txchen@tichecode.tichecode.d2.internal.cloudapp.net>
2014-03-20 22:52:08 +0000

    copy gitconfig in copy script

--- copy-configs.sh
+++ copy-configs.sh
@@ -4,5 +4,6 @@ cp -f my.zshrc ~/.zshrc || (echo "failed to cp my.zshrc"; exit 2)
 
 cp -f my.tmux.conf ~/.tmux.conf || (echo "failed to cp my.tmux.conf"; exit 2)
 cp -f vimrc ~/.vimrc || (echo "failed to cp vimrc"; exit 2)
+cp -f my.gitconfig ~/.gitconfig || (echo "failed to cp gitconfig"; exit 2)
 
 echo "config files are copied :)"
