7b6ef71f5004b976ed94aa5d2578dfb276118484
Tianxiang Chen <txchendev@gmail.com>
2016-02-21 21:48:00 -0800

    golang 1.6

--- setup-golang.sh
+++ setup-golang.sh
@@ -21,9 +21,9 @@ if [[ ! -w /usr/local ]] ; then
   err "cannot write to /usr/local, check your permission plz."
 fi
 
-package_name="go1.5.linux-386.tar.gz"
+package_name="go1.6.linux-386.tar.gz"
 if [[ $(arch) == x86_64 ]]; then
-  package_name="go1.5.linux-amd64.tar.gz"
+  package_name="go1.6.linux-amd64.tar.gz"
 fi
 download_url="https://storage.googleapis.com/golang/${package_name}"
 echo "## downloading ${download_url}"
