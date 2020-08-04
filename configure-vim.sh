97cd529b66e3bdd5813814d78a06a26d96169ce8
Tianxiang Chen <txchendev@gmail.com>
2020-08-03 20:58:02 -0700

    fix install script bug

--- configure-vim.sh
+++ configure-vim.sh
@@ -6,6 +6,7 @@ cp -f vim/vimrc ~/.vimrc || { echo "!! failed to cp vimrc"; exit 2; }
 echo "copy vimrc done"
 
 echo -e "\ncopy init.vim for neovim..."
+mkdir -p ~/.config/nvim
 cp -f nvim/init.vim ~/.config/nvim/init.vim || { echo "!! failed to cp init.vim"; exit 2; }
 echo "copy init.vim done"
 
