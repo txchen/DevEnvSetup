2e05f98a9d550af8805a10a8a867dd3080e6c8d6
Tianxiang Chen <txchendev@gmail.com>
2022-11-05 23:46:33 -0700

    Use tpm to manage tmux plugin, add themepack, auto persist session

--- configure-tmux.sh
+++ configure-tmux.sh
@@ -5,13 +5,11 @@ echo -e "\ncopying .tmux.conf..."
 cp -f tmux/my.tmux.conf ~/.tmux.conf || { echo "!! failed to cp my.tmux.conf"; exit 2; }
 echo "copy done"
 
-echo -e "\ninstalling tmux resurrect"
-TMUXRESURRECTDIR=~/.tmux-resurrect/
-if [ -d "$TMUXRESURRECTDIR" ]; then
-  rm -rf $TMUXRESURRECTDIR
+# install tmux plugin manager
+TPMDIR=~/.tmux/plugins/tpm
+if [ -d "$TPMDIR" ]; then
+  rm -rf $TPMDIR
 fi
-git clone https://github.com/tmux-plugins/tmux-resurrect.git $TMUXRESURRECTDIR || {
-  echo "!! failed to setup tmux resurrect"
-  exit 2
-}
-echo "tmux config done"
+git clone https://github.com/tmux-plugins/tpm $TPMDIR
+
+echo "tmux config done. Use 'C-b I' to install tpm plugins"
